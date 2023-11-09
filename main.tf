18abba73371cd0b2b4657bdb62745f31ce25ee9f
kishoresurapally <surapallykishore@gmail.com>
2023-11-09 22:46:08 +0530

    second commit

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "example" {
     Environment = "Dev"
   }
 }
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-2"
+
+  tags = {
+    Name        = "My bucket-2"
+    Environment = "Dev"
+  }
+}
+
